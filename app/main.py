c83d1886f5a269eb6fbb0cbc58f84e96d16ea26b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-15 20:52:56 +1000

    🎉 Update

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 import os
-import emoji
 import openai
 
-openai.api_key = os.geten("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def main():
-    pass
+    tweet = ""
+
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt="Generate a emoji that represents the following text:\n\n'I am very happy right now' : 😂\n'I am feeling really I want to go and see a movie tomorrow night Do you want to join me' : ",
+        max_tokens=60,
+    )
+
+    out = response["choices"][0]["text"]
+
+    print(out)
 
 if __name__ == "__main__":
     main()
