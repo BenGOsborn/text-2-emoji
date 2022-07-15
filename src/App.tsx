c024eaf41b88cd6074e57d3d4a4cbb8e1950a43e
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-15 23:21:40 +1000

    🎉 Update

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ function App() {
     const apiUrl = import.meta.env.VITE_API_URL;
     const apiKey = import.meta.env.VITE_API_KEY;
 
-    console.log(apiUrl, apiKey);
-
     const [emoji, setEmoji] = useState<string[] | null>(null);
 
     return (
