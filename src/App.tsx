95cf52d66ff0869cb7707d284bd9b1d54fb0252e
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-21 21:31:03 +1000

    🎉 Update

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,45 @@ function App() {
     const apiKey = import.meta.env.VITE_API_KEY;
 
     const [emoji, setEmoji] = useState<string[] | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
     return (
         <div className="mt-36 flex flex-col space-y-4">
             <h1 className="text-center text-2xl font-bold text-gray-900">💬 Text 2 Emoji 🤯</h1>
             <h2 className="text-center text-lg font-medium text-gray-800">Generate emojis that correspond to your message!</h2>
-            <p className="text-md font-medium text-gray-700">Simply enter your text below, then press "Generate Emojis", and then copy your emojis!</p>
+            <p className="text-center text-md font-medium text-gray-700">Simply enter your text below, press "Generate Emojis", and then copy your resulting emojis!</p>
             <form
                 className="flex flex-col space-y-3"
                 onSubmit={async (e) => {
                     e.preventDefault();
-                    const value = (e.target as any).text.value;
 
-                    const data = await submit(apiUrl, apiKey, value);
-                    setEmoji(data);
+                    if (!loading) {
+                        setLoading(true);
 
-                    // @ts-ignore
-                    e.target.reset();
+                        const value = (e.target as any).text.value;
+
+                        submit(apiUrl, apiKey, value)
+                            .then((data) => {
+                                setEmoji(data);
+
+                                // @ts-ignore
+                                e.target.reset();
+
+                                setLoading(false);
+                            })
+                            .catch(() => setLoading(false));
+                    }
                 }}
             >
                 <label htmlFor="text" className="text-lg font-bold text-gray-800">
                     Your Message
                 </label>
                 <textarea rows={4} className="rounded-md border-none bg-gray-800 font-medium text-base text-white" name="text" id="text" placeholder="I am going to the movies today..."></textarea>
-                <input className="bg-yellow-400 hover:bg-yellow-500 rounded-md p-3 font-bold text-lg text-gray-900 cursor-pointer" type="submit" value="Generate Emojis" />
+                {!loading ? (
+                    <input className="bg-yellow-400 hover:bg-yellow-500 rounded-md p-3 font-bold text-lg text-gray-900 cursor-pointer" type="submit" value="Generate Emojis" />
+                ) : (
+                    <input className="bg-yellow-500 rounded-md p-3 font-bold text-lg text-gray-900 cursor-default" type="submit" disabled={true} value="Loading Emojis..." />
+                )}
             </form>
             {emoji && (
                 <>
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <main className="w-1/3 mx-auto">
+        <main className="w-5/6 lg:w-1/2 xl:w-1/3 mx-auto">
             <App />
         </main>
     </React.StrictMode>
